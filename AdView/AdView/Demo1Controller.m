0bb14e3b593c1ec05ff8e55e654e1e295d85480f
Qianzhiyi <zhiyi@qingfengweb.com>
2015-07-05 19:01:04 +0800

    解决了一些BUG

--- AdView/AdView/Demo1Controller.m
+++ AdView/AdView/Demo1Controller.m
@@ -28,6 +28,8 @@
     
     NSArray *imagesURL = @[
                            @"https://ss0.baidu.com/-Po3dSag_xI4khGko9WTAnF6hhy/super/whfpf%3D425%2C260%2C50/sign=a41eb338dd33c895a62bcb3bb72e47c2/5fdf8db1cb134954a2192ccb524e9258d1094a1e.jpg",
+                           @"http://c.hiphotos.baidu.com/image/w%3D400/sign=c2318ff84334970a4773112fa5c8d1c0/b7fd5266d0160924c1fae5ccd60735fae7cd340d.jpg",
+                           @"http://www.5858baypgs.com/img/aHR0cDovL3BpYzE4Lm5pcGljLmNvbS8yMDEyMDEwNS8xMDkyOTU0XzA5MzE1MTMzOTExNF8yLmpwZw==.jpg"
                            ];
     
     // 情景三：图片配文字(可选)
